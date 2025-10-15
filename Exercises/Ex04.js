3a47e20e484ec5d417acb0ed2e5731a4434521bd
Leosatwork <leasmith@ucsc.edu>
2025-10-14 18:49:46 -0700

    stinky stinky button

---
+++ Exercises/Ex04.js
@@ -0,0 +1,23 @@
+
+let totalnumber = 0;
+
+// add button titled try to click me 
+
+$("#needy-button") .click(function(){
+    totalnumber = totalnumber + 3
+    let sentence = "I was clicked ";
+    let totalnumbers= sentence + totalnumber;
+    $("#needy-button").html(totalnumbers);
+})
+
+
+//when it is clicked 
+//add one to the total number
+
+//show the total numvber
+
+//on our 
+
+//show "clicked total number times"
+
+//a top limit
